e5e828b9d47bc0df82bd675f3191104fdf0d28cf
ferdynand <ferdynandergypramudani11@gmail.com>
2024-07-31 14:34:30 +0700

    add encoder

--- encoder.py
+++ encoder.py
@@ -1,1 +1,12 @@
-print("Hello world")
+# program enkripsi
+# ''.join([chr((ord(flag[i]) << 8) + ord(flag[i + 1])) for i in range(0, len(flag), 2)])
+
+flag = 'picoCTF{16_bits_inst34d_of_8_d52c6b93}'
+result = ''
+
+for i in range(0, len(flag), 2):
+    a = ord(flag[i]) << 8
+    b = ord(flag[i+1])
+    c = a + b
+    result += chr(c)
+    print(result)
