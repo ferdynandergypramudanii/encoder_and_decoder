60a66ec91f1a368236f747ab4c98a60eee9aac4b
ferdynand <ferdynandergypramudani11@gmail.com>
2024-07-31 22:05:29 +0700

    add-encoder-code

---
+++ decoder.py
@@ -0,0 +1,9 @@
+flag = '灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸彤㔲挶戹㍽'
+result = ''
+
+for i in range(0, len(flag), 1):
+    a = (ord(flag[i]) >> 8)
+    result += chr(a)
+    b = (ord(flag[i]) - ((ord(flag[i]) >> 8) << 8))    
+    result += chr(b)
+    print(result)
