30d56cf1caeaa404b55b4e86317a2a19aa017f29
ferdynand <ferdynandergypramudani11@gmail.com>
2024-08-01 14:00:31 +0700

    add-reverse4-script

---
+++ reverse4_script.py
@@ -0,0 +1,5 @@
+flag = 'ocip{FTC0l_I4_t5m_ll0m_y_y3n58a025e3ÿð}'
+#picoCTF{I_l05t_4ll_my_m0n3y_0a853e52}
+
+for i in range(0, len(flag), 4):
+    print(flag[i+3]+flag[i+2]+flag[i+1]+flag[i], end='')
